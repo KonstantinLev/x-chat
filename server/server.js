aa5e7d259362f53f9d120010f69c00cb4dfab72f
X-Stels <x-stels@bk.ru>
2019-02-01 18:14:34 +0300

    add send msg for test socket(on/emit)

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const socketIO = require('socket.io')
 const path = require('path');
+const http = require('http')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('IO connection')
+    socket.on('createMsg', (data) => {
+        console.log('socket:createMsg', data)
+        socket.emit('newMsg', {
+            text: data.input,
+            date: new Date()
+        })
+    })
+})
+
+server.listen(port, () => {
     console.log(`server has been started on port ${port}...`);
 });
