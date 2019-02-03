b0212abdb2f20359d1335745523fa2e2f1486e58
X-Stels <x-stels@bk.ru>
2019-02-03 15:00:44 +0300

    send messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,64 @@
 const socket = io()
 
+Vue.component('x-chat-message', {
+    props: ['message', 'user'],
+    template: `
+        <div class="message" :class="{'owner': message.id === user.id}">
+            <div class="message-content z-depth-1">
+                <span v-if="message.id !== user.id">{{message.name}}:</span>
+                {{message.text}}
+            </div>
+        </div>
+    `
+})
+
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        message: '',
+        messages: [],
+        user: {
+            name: '',
+            room: ''
+        }
+    },
+    methods: {
+        sendMessage(){
+            const message = {
+                text: this.message,
+                name: this.user.name,
+                id: this.user.id
+            }
+
+            socket.emit('msg:create', message, err => {
+                if(err){
+                    console.error(err)
+                } else {
+                    this.message = ''
+                }
+            })
+        },
+        initializeConn(){
+            socket.on('msg:new', message => {
+                this.messages.push(message)
+            })
+        }
+    },
+    created(){
+       const params = window.location.search.split('&')
+       const name = params[0].split('=')[1].replace('+', ' ')
+       const room = params[1].split('=')[1].replace('+', ' ')
+       this.user = {name, room};
+       console.log(this.user);
+    },
+    mounted(){
+        socket.emit('join', this.user, data => {
+            if(typeof data === 'string'){
+                console.error(data)
+            } else {
+                this.user.id = data.userId
+                this.initializeConn()
+            }
+        });
+    }
 })
--- server/server.js
+++ server/server.js
@@ -10,16 +10,37 @@ const app = express();
 const server = http.createServer(app)
 const io = socketIO(server)
 
+const message = (name, text, id) => ({name, text, id});
+
 app.use(express.static(publicPath));
 
 io.on('connection', socket => {
-    console.log('IO connection')
-    socket.on('createMsg', (data) => {
-        console.log('socket:createMsg', data)
-        socket.emit('newMsg', {
-            text: data.input,
-            date: new Date()
-        })
+    //console.log('IO connection')
+    socket.on('join', (user, callback) => {
+        if(!user.name || !user.room){
+            return callback('Enter valid user data!')
+        }
+        callback({userId: socket.id})
+        socket.emit('msg:new', message('Admin', `Welcome, ${user.name}!`))
+    })
+    socket.on('msg:create', (data, callback) => {
+
+        if(!data){
+            callback(`Message can't be empty`)
+        } else {
+            callback()
+            io.emit('msg:new', message(data.name, data.text, data.id))
+            // socket.emit('newMsg', {
+            //     text: data.input,
+            //     date: new Date()
+            // })
+        }
+
+        // console.log('socket:createMsg', data)
+        // socket.emit('newMsg', {
+        //     text: data.input,
+        //     date: new Date()
+        // })
     })
 })
 
